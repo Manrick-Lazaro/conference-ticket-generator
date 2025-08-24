25f256a8aa7e867a8d75626ba1dd2d0e84b65d93
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-24 09:30:36 -0300

    feat: import header component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p>vazio</p>
+      <Header />
     </div>
   );
 }
