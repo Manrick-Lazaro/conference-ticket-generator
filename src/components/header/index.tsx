30c794e33b9cfb0c1d0cc3ad563845891f1f32e6
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-24 09:33:26 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import Logo from "../../../public/images/logo-full.svg";
+
+export default function Header() {
+  return (
+    <header className="flex justify-center items-center py-9">
+      <Logo />
+    </header>
+  );
+}
