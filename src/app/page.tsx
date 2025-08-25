2b9d3d15819b6edc47d0c1b944fc3358d69b24aa
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-25 11:47:04 -0300

    feat: reorganize imports and adjust component structure in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Form from "@/components/form";
 import Header from "@/components/header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <Form />
     </div>
   );
 }
