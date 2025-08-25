33052ccc97f306a984e0ff21ff0f91a0afac73ab
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-25 11:47:00 -0300

    feat: update body structure with background images and layout adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="font-[inconsolata]">
+        <div className="fixed top-0 left-0 w-screen h-screen inset-0 -z-20 bg-[url('/images/background-mobile.png')] bg-cover bg-center" />
+
+        <div className="absolute inset-0 -z-10 bg-[url('/images/pattern-lines.svg')] bg-repeat bg-top" />
+
+        <div className="absolute z-10 bg-transparent w-full">{children}</div>
+      </body>
     </html>
   );
 }
