ab89b1085e7c00489e3554b9a41a0e801da422c1
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-26 11:08:12 -0300

    feat: add Hero component for improved layout and messaging

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <div className="w-full px-2 flex justify-center items-center">
+      <div
+        className="flex flex-col 
+      justify-center items-center max-w-[500px] px-2"
+      >
+        <h1
+          className="text-center font-bold text-c-neutral-0 text-3xl
+            mt-13 mb-7"
+        >
+          Your Journey to Coding Conf 2025 Starts Here!
+        </h1>
+        <p className="text-center text-xl text-c-neutral-300">
+          Secure your spot at next year&apos;s biggest coding conference
+        </p>
+      </div>
+    </div>
+  );
+}
