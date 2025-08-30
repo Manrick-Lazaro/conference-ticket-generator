5de86794bfbf7cea5556c31c91a796fdb3f7ef97
Manrick-Lazaro <manricksantos@gmail.com>
2025-08-30 11:14:30 -0300

    feat: refactor Ticket component to use zustand store for data management and image handling

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,22 +1,20 @@
+"use client";
+
+import Image from "next/image";
+
 import TicketSVG from "../../../public/images/pattern-ticket.svg";
 import LogoSVG from "../../../public/images/logo-mark.svg";
 import GithubSVG from "../../../public/images/icon-github.svg";
+import { useTicketStore } from "@/hooks/ticket";
+import { useEffect } from "react";
 
-type iticket = {
-  image: string;
-  fullName: string;
-  email: string;
-  github: string;
-  id: string;
-};
+export default function Ticket() {
+  const data = useTicketStore((s) => s.ticket);
 
-export default function Ticket({
-  email,
-  fullName,
-  github,
-  image,
-  id,
-}: iticket) {
+  useEffect(() => {
+    console.log(data);
+  });
+
   return (
     <div className="w-full px-2 flex justify-center items-center">
       <div
@@ -27,12 +25,12 @@ export default function Ticket({
           className="text-center font-bold text-c-neutral-0 text-3xl
             mt-13 mb-7"
         >
-          Congrats, <span>{fullName}!</span> Your ticket is ready.
+          Congrats, <span>{data?.fullName}!</span> Your ticket is ready.
         </h1>
 
         <p className="text-center text-xl text-c-neutral-300">
-          We&apos;ve emailed your ticket to <span>{email}</span> and will send
-          update in the run up to the event.
+          We&apos;ve emailed your ticket to <span>{data?.email}</span> and will
+          send update in the run up to the event.
         </p>
 
         <div className="mt-20 w-full h-fit aspect-[600/280] relative">
@@ -51,22 +49,33 @@ export default function Ticket({
               </div>
 
               <div className="flex justify-start gap-2">
-                <LogoSVG className="w-9 h-12 pt-2" />
+                {/* <LogoSVG className="w-9 h-12 pt-2" /> */}
+                <div className="bg-c-neutral-700/60 px-0.5 py-0.5 rounded-xl">
+                  <Image
+                    src={
+                      data !== null ? data.image : "/images/anonymous_user.png"
+                    }
+                    alt="preview"
+                    width={400}
+                    height={400}
+                    className="rounded-xl w-12 h-12 object-cover"
+                  />
+                </div>
                 <div className="flex flex-col justify-start items-start ">
-                  <h1 className="text-c-neutral-0 text-lg">Jonatan Kristof</h1>
+                  <h1 className="text-c-neutral-0 text-lg">{data?.fullName}</h1>
                   <p
                     className="text-c-neutral-300/80 text-xs flex 
                   justify-center gap-1"
                   >
                     <GithubSVG className="w-5 h-5" />
-                    {github}
+                    {data?.github}
                   </p>
                 </div>
               </div>
             </div>
 
             <div className="w-[17%] flex items-center justify-center">
-              <p className="text-c-neutral-300/60 rotate-90">#{id}</p>
+              <p className="text-c-neutral-300/60 rotate-90">#{data?.id}</p>
             </div>
           </div>
         </div>
